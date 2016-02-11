65319b9942d48a773286e4ad3e2a34fd6ba45eb1
Kevin <yangkevint@gmail.com>
2016-02-11 14:21:06 +0900

    style: code beautifier
    Removed empty files, formatted CSS, HTML, JS

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,3 +1,27 @@
 /**
  * Created by Kevin on 2/7/2016.
  */
+var gulp = require('gulp');
+var prettify = require('gulp-jsbeautifier');
+
+gulp.task('format-js', function() {
+    gulp.src('./src/js/main.js')
+        .pipe(prettify({indentSize: 4}))
+        .pipe(gulp.dest('./src/js/main.js'))
+});
+
+gulp.task('prettify-html', function() {
+    gulp.src('./src/index.html')
+        .pipe(prettify({indentSize: 4}))
+        .pipe(gulp.dest('./src/index.html'))
+});
+
+gulp.task('prettify-css', function() {
+    gulp.src('./src/css/style.css')
+        .pipe(prettify({indentSize: 4}))
+        .pipe(gulp.dest('./src/css/'))
+});
+
+gulp.task('default', function() {
+    gulp.start('format-js', 'prettify-html', 'prettify-css');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,9 @@
  */
 //global variable, so that it can be accessed by other functions.
 var map;
-
 //code taken from google maps API site
 //initializes map on start.
 var initMap = function() {
-
     //Preset style taken from https://snazzymaps.com/style/2/midnight-commander
     var styleArray = [{
         "featureType": "all",
@@ -101,7 +99,6 @@ var initMap = function() {
             "color": "#021019"
         }]
     }];
-
     // Create a map object and specify the DOM element for display.
     map = new google.maps.Map(document.getElementById('map'), {
         //centered around Taipei Main Station
@@ -112,7 +109,8 @@ var initMap = function() {
         zoomControl: true,
         zoomControlOptions: {
             position: google.maps.ControlPosition.LEFT_CENTER
-        },streetViewControl: true,
+        },
+        streetViewControl: true,
         streetViewControlOptions: {
             position: google.maps.ControlPosition.RIGHT_TOP
         },
@@ -122,14 +120,10 @@ var initMap = function() {
         styles: styleArray,
         zoom: 15
     });
-
-
 };
-
-var googleError = function(){
-  alert("There was a problem with Google and we can't load the map!");
+var googleError = function() {
+    alert("There was a problem with Google and we can't load the map!");
 };
-
 //global variables, to allow window closure
 var infowindow;
 var markerList = [];
@@ -138,13 +132,9 @@ var backup = [];
 
 function addMarker(location, name, review, link, id, map) {
     // Adds a marker with the information received from AJAX and Yelp API
-
-    var contentString = '<div id="content"><div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' +
-        '<div id="bodyContent"><p>' + review + '</p><p><a href=' + link + '/> Click for more info on Yelp! </a></div>' +
-        '</div>';
-
+    var contentString = '<div id="content"><div id="siteNotice">' + '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' + '<div id="bodyContent"><p>' + review +
+        '</p><p><a href=' + link + '/> Click for more info on Yelp! </a></div>' + '</div>';
     //initially places marker
     var marker = new google.maps.Marker({
         position: location,
@@ -153,7 +143,6 @@ function addMarker(location, name, review, link, id, map) {
         animation: google.maps.Animation.DROP,
         content: contentString
     });
-
     //this adds the click functionality, animation, info window
     marker.addListener('click', function() {
         infowindow.setContent(contentString);
@@ -163,35 +152,25 @@ function addMarker(location, name, review, link, id, map) {
             marker.setAnimation(null);
         }, 3000);
     });
-
     infowindow = new google.maps.InfoWindow({
         content: contentString
     });
-
     markerList.push(marker);
-
-
 }
-
-
 /* code adapted from
  https://github.com/levbrie/mighty_marks/blob/master/yelp-search-sample.html
  */
-
 //tried to encapsulate the authorization and ajax request into one.
 //first half is all OAuth, second half is ajax request. Authetnicate gets run at the bottom
 var Authenticate = function() {
-
     var self = this;
     self.terms = 'coffee';
     self.near = 'Taipei Main Station';
     self.best = '2';
     self.amount = '10';
     self.radius = '1000';
-
     var auth = {
         // Authentication tokens
-
         consumerKey: "IGfiXK01QiIcfiqaKDVR2A",
         consumerSecret: "57UqsYA2wVdVm6XRLMBo7GIcSKM",
         accessToken: "azapevIdp58SLt9wTU1NJ1JOD6Ybluiy",
@@ -200,12 +179,10 @@ var Authenticate = function() {
             signatureMethod: "HMAC-SHA1"
         }
     };
-
     var accessor = {
         consumerSecret: auth.consumerSecret,
         tokenSecret: auth.accessTokenSecret
     };
-
     self.parameters = [];
     parameters.push(['term', self.terms]);
     parameters.push(['location', self.near]);
@@ -222,13 +199,11 @@ var Authenticate = function() {
         'method': 'GET',
         'parameters': parameters
     };
-
     OAuth.setTimestampAndNonce(self.message);
     OAuth.SignatureMethod.sign(self.message, accessor);
     self.parameterMap = OAuth.getParameterMap(self.message.parameters);
     self.parameterMap.oauth_signature = OAuth.percentEncode(self.parameterMap.oauth_signature);
     console.log(self.parameterMap);
-
     //ajax request from YELP API, uses JSONP. Error checking is from setTimeout()
     $.ajax({
         'url': self.message.action,
@@ -239,54 +214,41 @@ var Authenticate = function() {
         'success': function(data) {
             console.log(data);
             var info = {};
-
             //runs through and parses data to display on the website.
             for (var i = 0; i < data.businesses.length; i++) {
-
                 this.location = {
                     lat: data.businesses[i].location.coordinate.latitude,
                     lng: data.businesses[i].location.coordinate.longitude
                 };
                 this.name = data.businesses[i].name;
-
                 //if snippet_text can not be found, returns no info found
                 if (data.businesses[i].snippet_text === undefined) {
                     this.snippet_text = "No info found!";
                 } else {
                     this.snippet_text = data.businesses[i].snippet_text;
                 }
-
                 this.url = data.businesses[i].url;
-
                 //adds marker on the screen
                 addMarker(this.location, this.name, this.snippet_text, this.url, i, map);
-
                 //makes observable/updating array using knockout.
                 coffeeShops.push({
                     id: ko.observable(i),
                     name: this.name,
                     snippet: this.snippet_text
                 });
-
                 //Error checking troubleshooting. Shows what returns in the array through console.
                 console.log(ko.toJSON(coffeeShops));
-
                 //on completion it clears the timeout.
                 clearTimeout(yelpRequestTimeout);
             }
-
-
         }
     });
-
     //simple setTimeout to check if the request comes back properly or not from YELP API
     var yelpRequestTimeout = setTimeout(function() {
         viewModel.errorMessage(true);
         console.log(true);
     }, 5000);
 };
-
-
 var viewModel = {
     self: this,
     data: ko.observableArray(),
@@ -296,62 +258,47 @@ var viewModel = {
     //SUPER IMPORTANT **, must store things in back up TWICE. once at the beginning and at the end. TO KEEP THE ARRAY FROM DESTRUCTING\
     //live search implementation from list view.
     search: function(value) {
-
         //used to check where my array was going!
         console.log(ko.toJSON(viewModel.properties));
-
         //makes a backup copy of info stored from AJAX request
         backup = coffeeShops.slice(0);
-
         //some more error checking. Make sure both of the arrays still exist.
         console.log(ko.toJSON(backup));
         console.log(ko.toJSON(coffeeShops));
-
         //super destructive removeAll. Completely clears memory of any array linked to that information.
         //You need th beackup, because of this.
         viewModel.properties.removeAll();
-
         //simple live search implementation
         //clears markers as well as list
         for (var i = 0; i < backup.length; i++) {
             if (backup[i].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
                 var thing = backup[i];
-
                 viewModel.properties.push(thing);
                 markerList[i].setVisible(true);
             } else {
                 markerList[i].setVisible(false);
             }
         }
-
         console.log(ko.toJSON(backup));
         coffeeShops = backup.splice(0);
-
     },
-    //refactored code. uses click to animate and link lists together. 
+    //refactored code. uses click to animate and link lists together.
     openWindows: function(data, event) {
         var self = this;
         this.id = event.currentTarget.id;
         console.log(self.id);
         infowindow.setContent(markerList[this.id].content);
         infowindow.open(map, markerList[this.id]);
-
         markerList[this.id].setAnimation(google.maps.Animation.BOUNCE);
         setTimeout(function() {
             markerList[self.id].setAnimation(null);
         }, 3000);
     }
-
 };
-
-
 //viewModel that updates the screen markers as well as list view.
 $(document).ready(function() {
-
-
     ko.applyBindings(viewModel);
     viewModel.query.subscribe(viewModel.search);
 });
-
 //runs and retrieves the info from YELP api
 Authenticate();
